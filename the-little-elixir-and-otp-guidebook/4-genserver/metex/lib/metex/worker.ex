407aa1ec74c43e005e53cbd45aa30941b4fda0a5
Alex Gascón <alexgascon.93@gmail.com>
2019-03-10 11:36:20 +0100

    Setting a custom name to the server

--- the-little-elixir-and-otp-guidebook/4-genserver/metex/lib/metex/worker.ex
+++ the-little-elixir-and-otp-guidebook/4-genserver/metex/lib/metex/worker.ex
@@ -1,26 +1,28 @@
 defmodule Metex.Worker do
   use GenServer
 
+  @name MW
+
   ## Client API
 
   def start_link(opts \\ []) do
-    GenServer.start_link(__MODULE__, :ok, opts)
+    GenServer.start_link(__MODULE__, :ok, opts ++ [name: @name])
   end
 
-  def get_temperature(pid, location) do
-    GenServer.call(pid, {:location, location})
+  def get_temperature(location) do
+    GenServer.call(@name, {:location, location})
   end
 
-  def get_stats(pid) do
-    GenServer.call(pid, :get_stats)
+  def get_stats do
+    GenServer.call(@name, :get_stats)
   end
 
-  def reset_stats(pid) do
-    GenServer.cast(pid, :reset_stats)
+  def reset_stats do
+    GenServer.cast(@name, :reset_stats)
   end
 
-  def stop(pid) do
-    GenServer.cast(pid, :stop)
+  def stop do
+    GenServer.cast(@name, :stop)
   end
 
   ## Server Callbacks
