696e5bffbcd64a0cf8835e6c185a27efe74e12a0
Alex Gascón <alexgascon.93@gmail.com>
2019-07-30 22:34:55 +0100

    Removing all Gradle files and starting again

---
+++ test-driven-development-by-example/src/test/MainTest.java
@@ -0,0 +1,13 @@
+package tdd;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+    @Test
+    public void testMultiplication() {
+        System.out.println("HEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEY")
+        assertEquals(10, 5 * 3);
+    }
+}
