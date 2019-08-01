025fa5c54d40dbeeca66eb95c49f68a5a65cfd2e
Alex Gascón <alexgascon.93@gmail.com>
2019-08-01 08:02:23 +0100

    Moving .times() to Money + creating Dollar and Franc factories
    These commits involve several changes, but the goal of all of them is
    the same: removing functionality from the subclasses and moving it into
    the main Money class.
    
    In this commit, we've done that in the following way:
    - Modifying the `.times()` method declaration to return `Money` objects
    instead of specifying the class directly
    - Making Money an abstract class, and declaring as abstract the
    `.times()` method
    - Creating the factory methods `.dollar()` and `.franc()`, that create a
    new instance of Dollar or Franc respectively, and stop using the
    subclasses directly in our code

--- test-driven-development-by-example/src/main/java/tdd/Franc.java
+++ test-driven-development-by-example/src/main/java/tdd/Franc.java
@@ -5,7 +5,7 @@ public class Franc extends Money {
         this.amount = amount;
     }
 
-    Franc times(int multiplier) {
+    Money times(int multiplier) {
         return new Franc(amount * multiplier);
     }
 }
--- test-driven-development-by-example/src/main/java/tdd/Money.java
+++ test-driven-development-by-example/src/main/java/tdd/Money.java
@@ -1,10 +1,20 @@
 package tdd;
 
-public class Money {
+abstract public class Money {
     protected int amount;
+
+    static Dollar dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    static Franc franc(int amount) {
+        return new Franc(amount);
+    }
 
     public boolean equals(Object object) {
         Money money = (Money) object;
         return amount == money.amount && getClass().equals(money.getClass());
     }
+
+    abstract Money times(int multiplier);
 }
