3c7ed28b40b5f13a84b6e619afc45198f30c256d
Alex Gascón <alexgascon.93@gmail.com>
2019-08-03 15:12:24 +0100

    Implementing rates correctly
    Implementing the methods `addRate` and `rate` to add and retrieve rates
    from a Bank respectively.
    
    These method have been implemented using a Hashtable, in which the key
    is a `Pair` representing a pair of currencies and the key is an integer
    (the rate).
    
    As Pair needs to be hashable to be used as a key, we have implemented
    both the `equals` and the `hashCode` methods for it. However, we have
    set the hashcode to always be 0 for simplicity, and because due to the
    fact that we won't have much use-cases yet the performance decrease won't
    be noticeable

--- test-driven-development-by-example/src/main/java/tdd/Bank.java
+++ test-driven-development-by-example/src/main/java/tdd/Bank.java
@@ -1,13 +1,20 @@
 package tdd;
 
+import java.util.Hashtable;
+
 public class Bank {
+    private Hashtable<Pair, Integer> rates = new Hashtable<Pair, Integer>();
+
     Money reduce(Expression source, String to) {
         return source.reduce(this, to);
     }
 
     int rate(String from, String to) {
-        return (from.equals("CHF") && to.equals("USD")) ? 2 : 1;
+        if (from.equals(to)) return 1;
+        return rates.get(new Pair(from, to));
     }
 
-    void addRate(String from, String to, int rate) { }
+    void addRate(String from, String to, int rate) {
+        rates.put(new Pair(from, to), rate);
+    }
 }
