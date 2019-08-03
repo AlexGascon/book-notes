466de64cfdc69c11f6f3d251219f3f094640d8ca
Alex Gascón <alexgascon.93@gmail.com>
2019-08-03 15:28:30 +0100

    Implementing mixed addition
    Modifying the method `Sum.reduce` to make sure that it reduces the
    operands to a common currency before adding them

--- test-driven-development-by-example/src/test/java/tdd/MoneyTest.java
+++ test-driven-development-by-example/src/test/java/tdd/MoneyTest.java
@@ -66,4 +66,14 @@ public class MoneyTest {
         Money result = bank.reduce(Money.franc(2), "USD");
         assertEquals(Money.dollar(1), result);
     }
+
+    @Test
+    public void testMixedAddition() {
+        Money fiveDollars = Money.dollar(5);
+        Money tenFrancs = Money.franc(10);
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Money result = bank.reduce(fiveDollars.plus(tenFrancs), "USD");
+        assertEquals(Money.dollar(10), result);
+    }
 }
