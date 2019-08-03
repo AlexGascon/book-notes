b46bfdb71bc3f0b8ddb2169194b7dbf4dff4bf2f
Alex Gascón <alexgascon.93@gmail.com>
2019-08-03 15:02:54 +0100

    Changing the Expression.reduce signature
    We don't want the `Expression` objects to be aware of the change rate logic,
    so we'll modify the method to receive a bank as an argument. This bank
    will return the rate that will be used to change currencies

--- test-driven-development-by-example/src/main/java/tdd/Bank.java
+++ test-driven-development-by-example/src/main/java/tdd/Bank.java
@@ -2,7 +2,7 @@ package tdd;
 
 public class Bank {
     Money reduce(Expression source, String to) {
-        return source.reduce(to);
+        return source.reduce(this, to);
     }
 
     int rate(String from, String to) {
--- test-driven-development-by-example/src/main/java/tdd/Money.java
+++ test-driven-development-by-example/src/main/java/tdd/Money.java
@@ -30,8 +30,9 @@ public class Money implements Expression {
         return amount + " " + currency;
     }
 
-    public Money reduce(String to) {
-        return this;
+    public Money reduce(Bank bank, String to) {
+        int rate = bank.rate(currency, to);
+        return new Money(amount / rate, to);
     }
 
     Money times(int multiplier) {
