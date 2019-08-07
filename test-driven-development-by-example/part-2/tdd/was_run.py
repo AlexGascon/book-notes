50ba4ddd8b3551f669f475e5e2f0a14a0388b9b1
Alex Gascón <alexgascon.93@gmail.com>
2019-08-07 08:53:19 +0100

    Refactor - Deleting unnecessary flags
    Deleting some of the flags that we set initially to find out if our
    methods had run or not, as we forgot to remove them when we remove the
    rest of the related code

--- test-driven-development-by-example/part-2/tdd/was_run.py
+++ test-driven-development-by-example/part-2/tdd/was_run.py
@@ -3,12 +3,10 @@ from tdd.test_case import TestCase
 
 class WasRun(TestCase):
     def set_up(self):
-        self.was_run = False
         self.log = "set_up "
 
     def tear_down(self):
         self.log += "tear_down "
 
     def test_method(self):
-        self.was_run = True
         self.log += "test_method "
