53fddaf431b66a7a94fa9646fa73e96822d765cf
Alex Gascón <alexgascon.93@gmail.com>
2019-08-07 08:14:52 +0100

    TDD - Testing set up

--- test-driven-development-by-example/part-2/test/tests.py
+++ test-driven-development-by-example/part-2/test/tests.py
@@ -9,4 +9,10 @@ class TestCaseTest(TestCase):
         test.run()
         assert(test.was_run)
 
+    def test_set_up(self):
+        test = WasRun("test_method")
+        test.run()
+        assert(test.was_set_up)
+
 TestCaseTest("test_running").run()
+TestCaseTest("test_set_up").run()
