210669d9d4cd6ddc1083173984173ba9cc2d88eb
Alex Gascón <alexgascon.93@gmail.com>
2019-08-08 22:21:43 +0100

    Green - Keeping count of the test failures
    Implementing the test_failed method to count the number of failures that
    we have in our tests, and using that value in summary to report the test
    results

--- test-driven-development-by-example/part-2/tdd/test_result.py
+++ test-driven-development-by-example/part-2/tdd/test_result.py
@@ -1,9 +1,13 @@
 class TestResult:
     def __init__(self):
         self.run_count = 0
+        self.failure_count = 0
 
     def test_started(self):
         self.run_count += 1
 
+    def test_failed(self):
+        self.failure_count += 1
+
     def summary(self):
-        return f"{self.run_count} run, 0 failed"
+        return f"{self.run_count} run, {self.failure_count} failed"
