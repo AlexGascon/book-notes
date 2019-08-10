372456a6a9201d290d5059a9592f846f99a0fcfd
Alex Gascón <alexgascon.93@gmail.com>
2019-08-10 17:32:05 +0100

    Refactor - Result in set_up
    As now all the test repeat the TestResult initialization, we're going to
    move it to the set_up part of the test class to avoid unnecessary code
    repetition

--- test-driven-development-by-example/part-2/test/tests.py
+++ test-driven-development-by-example/part-2/test/tests.py
@@ -5,37 +5,35 @@ from tdd.was_run import WasRun
 
 
 class TestCaseTest(TestCase):
+    def set_up(self):
+        self.result = TestResult()
+
     def test_template_method(self):
         test = WasRun("test_method")
-        result = TestResult()
-        test.run(result)
+        test.run(self.result)
         assert("set_up test_method tear_down " == test.log)
  
     def test_result(self):
         test = WasRun("test_method")
-        result = TestResult()
-        test.run(result)
-        assert("1 run, 0 failed" == result.summary())
+        test.run(self.result)
+        assert("1 run, 0 failed" == self.result.summary())
 
     def test_failed_result(self):
         test = WasRun("test_broken_method")
-        result = TestResult()
-        test.run(result)
-        assert("1 run, 1 failed" == result.summary())
+        test.run(self.result)
+        assert("1 run, 1 failed" == self.result.summary())
 
     def test_failed_result_formatting(self):
-        result = TestResult()
-        result.test_started()
-        result.test_failed()
-        assert("1 run, 1 failed" == result.summary())
+        self.result.test_started()
+        self.result.test_failed()
+        assert("1 run, 1 failed" == self.result.summary())
 
     def test_suite(self):
         suite = TestSuite()
         suite.add(WasRun("test_method"))
         suite.add(WasRun("test_broken_method"))
-        result = TestResult()
-        suite.run(result)
-        assert("2 run, 1 failed" == result.summary())
+        suite.run(self.result)
+        assert("2 run, 1 failed" == self.result.summary())
 
 suite = TestSuite()
 suite.add(TestCaseTest("test_template_method"))
